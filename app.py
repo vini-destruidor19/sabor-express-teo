35173f9a1d43d8d19dd12e59c0fd8a837d9e8f66
Vini <134401761+vini-destruidor19@users.noreply.github.com>
2024-08-23 07:43:13 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,20 @@
+print("""
+      𝓘𝓶𝓹𝓮́𝓻𝓲𝓸 𝓓𝓸 𝓢𝓸𝓻𝓿𝓮𝓽𝓮
+      """)
+
+print("1. cadrastrar ")
+print("2. historia")
+print("3.lojas")
+print("4. produtos")
+print("5.sair")
+
+opcao_escolhida = input("Escolha uma opção:")
+print(f"você escolheu a opção {opcao_escolhida}")
+
+
+
+
+
+
+
+
